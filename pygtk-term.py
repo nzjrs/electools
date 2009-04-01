16fb74a0733563c9d1dbfdf302c0038251b0cfd4
John Stowers <john@nzjrs-desktop.(none)>
2009-04-02 00:03:52 +1300

    Add entry, and only show hexview in expander

--- pygtk-term.py
+++ pygtk-term.py
@@ -18,11 +18,27 @@ class UI:
         term = vte.Terminal()
         vb.pack_start(term, expand=True, fill=True)
 
-        hv = hexview.HexView()
-        vb.pack_start(hv, expand=False, fill=False)
+        entry = rawentry.MyEntry()
+        vb.pack_start(entry, expand=False, fill=False)
+
+        #store the hex view in an expander, only create said view
+        #when revealed the first time
+        expander = gtk.expander_new_with_mnemonic("_Hex View")
+        expander.connect("notify::expanded", self._expander_callback)
+        vb.pack_start(expander, expand=False, fill=False)
+        self.hv = None
 
         w.connect('delete-event', lambda *w: gtk.main_quit())
         w.show_all()
+
+    def _expander_callback(self, expander, *args):
+        if expander.get_expanded():
+            if not self.hv:
+                self.hv = hexview.HexView()
+                expander.add(self.hv)
+            expander.show_all()
+        else:
+            self.hv.hide_all()
 
 if __name__ == "__main__":
     u = UI()
